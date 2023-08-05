628d6b2a7cb059b165dcac3a0d8273bd8a5fba71
Philipe Vieira <philipemosv@gmail.com>
2023-08-05 18:20:54 -0300

    fix get tags

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from tags import tags
 def get_values(tag):
     """Get the values of a tag."""
     found_tag = soup.find(
-        lambda t: t.name == tag['tag'] and t.text.lower() == tag['text'])
+        lambda t: t.name == tag['tag']
+        and t.text.lower().strip("[]") == tag['text'])
     found_tag_siblings = found_tag.find_next_siblings()
     values = []
     for tag_sibling in found_tag_siblings:
